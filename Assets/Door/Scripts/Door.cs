60cd3629a886340a9aea45f7f5e62663975ac44a
Artur Morelle Nista <arturnista@gmail.com>
2019-10-05 17:24:05 -0300

    maps and door

---
+++ Assets/Door/Scripts/Door.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    
+    [SerializeField]
+    private Sprite closeSprite;
+    [SerializeField]
+    private Sprite openSprite;
+
+    private SpriteRenderer spriteRenderer;
+    
+    private GameObject closeCollider;
+    private GameObject openCollider;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        closeCollider = transform.Find("CloseCollider").gameObject;
+        openCollider = transform.Find("OpenCollider").gameObject;
+
+        Close();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Open()
+    {
+        spriteRenderer.sprite = openSprite;
+        openCollider.SetActive(true);
+        closeCollider.SetActive(false);
+    }
+
+    public void Close()
+    {
+        spriteRenderer.sprite = closeSprite;
+        openCollider.SetActive(false);
+        closeCollider.SetActive(true);
+    }
+}
